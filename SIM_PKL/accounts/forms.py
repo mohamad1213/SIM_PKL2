0c36e893af783b0ea8008f0161b89d39a0a7d18d
mohamad1213 <hatami391998@gmail.com>
2020-11-12 12:12:29 +0700

    log and reg

--- SIM_PKL/accounts/forms.py
+++ SIM_PKL/accounts/forms.py
@@ -10,3 +10,13 @@ class CreateUserForm(UserCreationForm):
 		model = User
 		fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['username'].widget.attrs.update({ 'class': 'input100', 'placeholder': 'Username..' })
+		self.fields['email'].widget.attrs.update({ 'class': 'input100', 'placeholder': 'Email..' })
+		self.fields['first_name'].widget.attrs.update({ 'class': 'input100', 'placeholder': 'Firstname..' })
+		self.fields['last_name'].widget.attrs.update({ 'class': 'input100', 'placeholder': 'Lastname..' })
+		self.fields['password1'].widget.attrs.update({ 'class': 'input100', 'placeholder': 'Enter Password..','id':'myInput'})
+		self.fields['password2'].widget.attrs.update({ 'class': 'input100', 'placeholder': 'Re-enter Password..' })
+
+
