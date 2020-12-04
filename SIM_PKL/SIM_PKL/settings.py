3302eb4d853a557328c3af4f4aa132fbea1f1a57
mohamad1213 <hatami391998@gmail.com>
2020-12-04 13:40:54 +0700

    adding email

--- SIM_PKL/SIM_PKL/settings.py
+++ SIM_PKL/SIM_PKL/settings.py
@@ -120,6 +120,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'hatami391998@gmail.com'
+EMAIL_HOST_PASSWORD = 'istimewajogja123'
 
 STATIC_URL = '/static/'
 
